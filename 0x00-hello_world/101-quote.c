41a60ecf6ba25753168693b316681b405d21f46d
UMAR YA'ABA ZUBAIRU <umarzubairuyaaba@gmail.com>
2022-09-10 00:18:20 -0700

    intel syntax

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
-/**
- * main - prints the std
-  error sandwiched around the standard output
- *
- * Return: returns 1
- **/
+/*
+ * main - prints exactly and that piece of art is useful
+ * - Dora Korpar, 2015-10-19
+ * followed by a new line, to the standard error.
+ * Return: Always  (Success)
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
