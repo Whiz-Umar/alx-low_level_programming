ba26bdc48ed1aa3172dc6a1adb21867d846b59eb
UMAR YA'ABA ZUBAIRU <umarzubairuyaaba@gmail.com>
2022-09-13 09:04:29 -0700

    Printing alphabets using user-defined function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_alphabet - Prints lowercase alphabets
+*
+* return: void
+*
+*/
+
+void print_alphabet(void)
+{
+	int letter = 97;
+
+	while (letter < 123)
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar('\n');
+
+	return (void);
+}
