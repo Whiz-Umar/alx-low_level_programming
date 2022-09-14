599405c13f97c074bdc0a4fdfb6f254791d50475
UMAR YA'ABA ZUBAIRU <umarzubairuyaaba@gmail.com>
2022-09-13 22:06:27 -0700

    Addi go two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+* add - This function adds two intergers and returns the result
+*
+* @a: The first number
+* @b: The second number
+*
+* Return: the addition of n1 and n2
+*/
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
