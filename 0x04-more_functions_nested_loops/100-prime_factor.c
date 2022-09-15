fc26e6cf0071c4ab8d7dabef18e751fe0eba6bd2
UMAR YA'ABA ZUBAIRU <umarzubairuyaaba@gmail.com>
2022-09-15 16:52:28 -0700

    Finding and printing the largest prime factor of a number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Finds and prints the largest prime factor of the number 612852475143
+*
+* Return: void
+*/
+
+int main(void)
+{
+	long n = 612852475143;
+	long divisor, largest;
+
+	divisor = 2;
+	while (n != 0)
+	{
+		if (n % divisor != 0)
+		{
+			++divisor;																		}
+		else
+		{
+			largest = n;
+			n = n / divisor;
+			if (n == 1)
+				break;
+		}
+	}
+	printf("%ld\n", largest);
+	return (0);
+}
