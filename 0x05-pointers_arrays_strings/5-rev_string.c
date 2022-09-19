38817ca9074a0df62309817da80c829bb15bec7d
CyberPath° <111005507+Whiz-Umar@users.noreply.github.com>
2022-09-19 21:34:22 +0100

    Function to print strings in reverse order

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string in reverse order followed by a new line
+ *
+ * @s: The string to be printed
+ *
+ * Returns: void
+ */
+
+void rev_string(char *s)
+{
+	int counter = _strlen(s) - 1;
+
+	while (s[counter] != 0)
+	{
+		_putchar(s[counter]);
+		counter--;
+	}
+	_putchar('\n');
+}
+
+
+/**
+ * _strlen - This function evaluates the length of a given string.
+ *
+ *@s: The string to be evaluated
+ *
+ * Return: Length of s
+ */
+
+int _strlen(char *s)
+{
+	long int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+return (length);
+}
