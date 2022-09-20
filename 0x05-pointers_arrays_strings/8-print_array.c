f055d22ffe1fd15af7ae3e91ecf689004806d287
CyberPath° <111005507+Whiz-Umar@users.noreply.github.com>
2022-09-20 05:05:17 +0100

    Printing the elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * print_array - prints n elements of an array of integers followed by a new line
+ * print_array - prints n elements of an array of integers
+ * followed by a new line
  *
  * @a: points to an array of integers
  * @n: total number of elements in &a to be printed
- * @counter: array elements counter
  *
  * Return: void
  */
