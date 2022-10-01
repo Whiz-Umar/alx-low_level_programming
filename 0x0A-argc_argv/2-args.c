1690ebec3861f9bbead10e04541dbf91745aad75
CyberPath° <111005507+Whiz-Umar@users.noreply.github.com>
2022-10-01 05:24:40 +0100

    Created a program that prints all args received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives.
+ *
+ * @argc: The argument count
+ * @argv: The argument vector pointer
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+		printf("%s\n", argv[i++]);
+
+	return (0);
+}
