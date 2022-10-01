e43e4a58425d2724380bb47e0be6a05db71ab6df
CyberPath° <111005507+Whiz-Umar@users.noreply.github.com>
2022-10-01 04:55:59 +0100

    Created a program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,9 @@
  * Return: 0
  */
 
-int main(int argc, char  *argv[])
+int main(int argc __attribute__((unused)), char  *argv[])
 {
-	__attribute__((unused))argc;
+	/*argc;*/
 printf("My name is %s\n", argv[0]);
 	return (0);
 }
