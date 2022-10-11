ab7386087ef3bb9cc398d2f1d5c01d5f2a66cdae
CyberPath° <111005507+Whiz-Umar@users.noreply.github.com>
2022-10-11 04:36:20 +0100

    Created a new struct type dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * struct dog - dog data
@@ -15,8 +13,12 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+} dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 
 #endif
