e1863f3621ec87e5cd7f27b5bec59e8031d3fb02
CyberPath° <111005507+Whiz-Umar@users.noreply.github.com>
2022-10-11 04:17:11 +0100

    Created a new struct type dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog data
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+#endif
