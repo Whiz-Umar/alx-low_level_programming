50e43ecf4c916902cb95e10e962a3bd42cf05b8f
CyberPath° <111005507+Whiz-Umar@users.noreply.github.com>
2022-10-13 15:04:54 +0100

    Wrote a function that prints numbers plus newline

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers, followed by a new line
+ *
+ * @separator: charcter used to seperate numbers
+ * @n: number of numbers to print
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, x;
+	va_list nums;
+
+	va_start(nums, n);
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(nums, int);
+		printf("%i", x);
+		if (i < (n - 1) && separator !=  NULL)
+			printf("%s", separator);
+	}
+	va_end(nums);
+	printf("\n");
+}
